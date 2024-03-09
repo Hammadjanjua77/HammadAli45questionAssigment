d47c2ad6c0e91e669d68d62de52fecaf5ab21418
HammadJanjua77 <160199063+Hammadjanjua77@users.noreply.github.com>
2024-03-09 23:41:00 +0500

    Update alien_colors_2.ts

--- 27. Alien colors#3/alien_colors_2.ts
+++ 27. Alien colors#3/alien_colors_2.ts
@@ -6,16 +6,12 @@
 // Using switch statement
 alienColor = "yellow"; // alienColor ka variable V1 file me Declared he.
 
-switch (alienColor) {
-    case "green":
-        console.log("The player earned 5 points.");
-        break;
-    case "yellow":
-        console.log("The player earned 10 points.");
-        break;
-    case "red":
-        console.log("The player earned 15 points.");
-        break;
-    default:
-        console.log("Unknown alien color."); // If not filled        
+if (alienColor === "green") {
+    console.log("The player earned 5 points.");
+} else if (alienColor === "yellow") {
+    console.log("The player earned 10 points.");
+} else if (alienColor === "red") {
+    console.log("The player earned 15 points.");
+} else {
+    console.log("Unknown alien color."); // If not filled.
 }
