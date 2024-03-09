58fc33ce9bfe3810e659b2da20306be28b5a66ee
HammadJanjua77 <160199063+Hammadjanjua77@users.noreply.github.com>
2024-03-09 23:47:55 +0500

    Update alien_colors_2.js

--- 27. Alien colors#3/alien_colors_2.js
+++ 27. Alien colors#3/alien_colors_2.js
@@ -1,18 +1,15 @@
-//Written by: Hammad Janjua Rajput
-// date : 19-feb-2024
-// Part 2
+"use strict";
 // Using switch statement
-alienColor = "yellow"; // alienColor ka variable V1 file me Declared he.
-switch (alienColor) {
-    case "green":
-        console.log("The player earned 5 points.");
-        break;
-    case "yellow":
-        console.log("The player earned 10 points.");
-        break;
-    case "red":
-        console.log("The player earned 15 points.");
-        break;
-    default:
-        console.log("Unknown alien color."); // If not filled        
+let alienColor = "yellow"; // alienColor ka variable V1 file me Declared he.
+if (alienColor === "green") {
+    console.log("The player earned 5 points.");
+}
+else if (alienColor === "yellow") {
+    console.log("The player earned 10 points.");
+}
+else if (alienColor === "red") {
+    console.log("The player earned 15 points.");
+}
+else {
+    console.log("Unknown alien color."); // If not filled.
 }
